92488759d15216bc8185bab4d62280b7ed9f2069
jinwoo <wlsdn0310@gmail.com>
2021-01-20 13:39:45 +0900

    version 20210120

---
+++ members/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Members
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>hello</h1>");
+
+def test(req):
+    return HttpResponse("<h2>Test</h2>")
+
+def signup(req):
+    if req.method == 'POST' :
+        username = req.POST['username']
+        email = req.POST['email']
+
+        #username == exit
+        #h2로 나가기를 출력
+        #if username == exit :
+           # return HttpResponse("나가기")
+        #elif username == 'codingon' :
+            #return render(req, 'login.html')
+
+        member = Members(
+            username = username,
+            useremail = email
+        )
+
+        member.save()
+        res_data = {}
+
+        
+        res_data['res'] = '등록성공'
+
+        return render(req, 'index.html', res_data)
+    return render(req, 'index.html')
